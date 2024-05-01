e90e417d6e7fac25ec849386c3bc1448a2047c3d
thomas1996 <thomas.kantahck@gmail.com>
2024-05-01 16:57:33 +0200

    add middlewar in production file

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,49 @@
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "default-src": ["'self'"],
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://knitmediaspace.fra1.digitaloceanspaces.com",
+            "https://market-assets.strapi.io",
+            ".digitaloceanspaces.com",
+            "https://knit-back-jds8b.ondigitalocean.",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://knitmediaspace.fra1.digitaloceanspaces.com",
+            "https://market-assets.strapi.io",
+            "https://knit-back-jds8b.ondigitalocean.",
+            "market-assets.strapi.io"
+          ],
+
+          upgradeInsecureRequests: null,
+        },
+      },
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:5173",
+        "https://knit-back-jds8b.ondigitalocean.app",
+      ],
+      headers: "*",
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
